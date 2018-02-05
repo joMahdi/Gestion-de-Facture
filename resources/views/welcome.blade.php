10ea47c3cf81a7bd319563135422fd343c2fb57e
joMahdi <joulali.mehdi@gmail.com>
2018-02-05 02:03:06 +0000

    Init Project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+       <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+    </head>
+    <body>
+        <div id="root">
+            <h1>mahdi</h1>
+        </div>
+    </body>
+        <script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+</html>
